4113cbfa668997934399c15395f89405dc60589e
Furkan-Gulsen <m.furkangulsen@gmail.com>
2019-11-17 09:36:30 +0300

    markdown

---
+++ Markdown Editor/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import App from "./App2";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
