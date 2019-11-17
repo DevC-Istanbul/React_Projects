c088cf61ecb26bd495286ce0be180ff088af567a
Codeblogger <41703336+Furkan-Gulsen@users.noreply.github.com>
2019-11-17 09:46:09 +0300

    Update index.js

--- Markdown Editor/src/index.js
+++ Markdown Editor/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-import App from "./App2";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
